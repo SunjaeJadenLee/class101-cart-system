1e136a9b4f36f94c955f29c5d277da730dee7be2
SunjaeJadenLee <sj.jaden1118@gmail.com>
2021-02-21 18:09:42 +0900

    feature: navigation 모듈 추가

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import {NavigationContainer} from '@react-navigation/native'
+import ProductProvider from './src/provider/ProductProvider';
 import React from 'react';
+import Routes from './src/presentation/util/Routes';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  
   return (
-    <></>
+    <>
+      <SafeAreaView />
+      <Routes />
+    </>
   );
 };
 
